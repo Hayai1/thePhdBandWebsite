ba2eda92236c7dac17c0dcf8ed1d27a2fb700b00
dylan hough <dylanpjhough@gmail.com>
2025-05-19 17:30:49 +0100

    debug = false for production

--- thePhd/thePhd/settings.py
+++ thePhd/thePhd/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(2dq42+e1&4ro(t1(+x!8x51blq0$@s-5hg8q=w-$vt*u8y$mc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1',
                  'localhost',
